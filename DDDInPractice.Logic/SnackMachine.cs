46a24ff6eb8d424356b21994df82cf272112e573
Josep Lluís Ribas <jlribas@girosystem.com>
2023-03-25 12:58:31 +0100

    define SnackMachine properties and methods

--- DDDInPractice.Logic/SnackMachine.cs
+++ DDDInPractice.Logic/SnackMachine.cs
@@ -2,6 +2,61 @@
 {
     public sealed class SnackMachine
     {
+        public int OneCentCount { get; private set; }
+        public int TenCentCount { get; private set;  }
+        public int QuarterCentCount { get; private set; }
+        public int OneDollarCount { get; private set; }
+        public int FiveDollarCount { get; private set; }
+        public int TwentyDollarCount { get; private set; }
 
+        public int OneCentCountInTransaction { get; private set; }
+        public int TenCentCountInTransaction { get; private set; }
+        public int QuarterCentCountInTransaction { get; private set; }
+        public int OneDollarCountInTransaction { get; private set; }
+        public int FiveDollarCountInTransaction { get; private set; }
+        public int TwentyDollarCountInTransaction { get; private set; }
+
+        public void InsertMoney(
+            int oneCentCount,
+            int tenCentCount,
+            int quartertCentCount,
+            int oneDollarCount,
+            int fiveDollarCount,
+            int twentyDollarCount)
+        {
+            OneCentCountInTransaction += oneCentCount;
+            TenCentCountInTransaction += tenCentCount;
+            QuarterCentCountInTransaction += quartertCentCount;
+            OneDollarCountInTransaction += oneDollarCount;
+            FiveDollarCountInTransaction += fiveDollarCount;
+            TwentyDollarCountInTransaction += twentyDollarCount;
+        }
+
+        public void ReturnMoney()
+        {
+            OneCentCountInTransaction = 0;
+            TenCentCountInTransaction = 0;
+            QuarterCentCountInTransaction = 0;
+            OneDollarCountInTransaction = 0;
+            FiveDollarCountInTransaction = 0;
+            TwentyDollarCountInTransaction = 0;
+        }
+
+        public void BuySnack()
+        {
+            OneCentCount += OneCentCountInTransaction;
+            TenCentCount += TenCentCountInTransaction;
+            QuarterCentCount += QuarterCentCountInTransaction;
+            OneDollarCount +=  OneDollarCountInTransaction;
+            FiveDollarCount +=  FiveDollarCountInTransaction;
+            TwentyDollarCount += TwentyDollarCountInTransaction;
+
+            OneCentCountInTransaction = 0;
+            TenCentCountInTransaction = 0;
+            QuarterCentCountInTransaction = 0;
+            OneDollarCountInTransaction = 0;
+            FiveDollarCountInTransaction = 0;
+            TwentyDollarCountInTransaction = 0;
+        }
     }
 }
