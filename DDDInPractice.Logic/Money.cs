6f7c91c7821f12198cc4abffd03c7e1d01e87014
Josep Lluís Ribas <jlribas@girosystem.com>
2023-03-25 13:09:07 +0100

    add Money class abstraction

---
+++ DDDInPractice.Logic/Money.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDInPractice.Logic
+{
+    public sealed class Money
+    {
+        public int OneCentCount { get; set; }
+        public int TenCentCount { get; set; }
+        public int QuarterCentCount { get; set; }
+        public int OneDollarCount { get; set; }
+        public int FiveDollarCount { get; set; }
+        public int TwentyDollarCount { get; set; }
+
+        public Money(int oneCentCount, int tenCentCount, int quarterCentCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
+        {
+            OneCentCount = oneCentCount;
+            TenCentCount = tenCentCount;
+            QuarterCentCount = quarterCentCount;
+            OneDollarCount = oneDollarCount;
+            FiveDollarCount = fiveDollarCount;
+            TwentyDollarCount = twentyDollarCount;
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            Money sum = new Money(
+                money1.OneCentCount + money2.OneCentCount,
+                money1.TenCentCount + money2.TenCentCount,
+                money1.QuarterCentCount + money2.QuarterCentCount,
+                money1.OneDollarCount + money2.OneDollarCount,
+                money1.FiveDollarCount + money2.FiveDollarCount,
+                money1.TwentyDollarCount + money2.TwentyDollarCount
+                );
+
+            return sum;
+        }
+    }
+}
--- DDDInPractice.Logic/SnackMachine.cs
+++ DDDInPractice.Logic/SnackMachine.cs
@@ -2,61 +2,24 @@
 {
     public sealed class SnackMachine
     {
-        public int OneCentCount { get; private set; }
-        public int TenCentCount { get; private set;  }
-        public int QuarterCentCount { get; private set; }
-        public int OneDollarCount { get; private set; }
-        public int FiveDollarCount { get; private set; }
-        public int TwentyDollarCount { get; private set; }
+        public Money MoneyInside { get; private set; }
+        public Money MoneyInTransaction { get; private set; }
 
-        public int OneCentCountInTransaction { get; private set; }
-        public int TenCentCountInTransaction { get; private set; }
-        public int QuarterCentCountInTransaction { get; private set; }
-        public int OneDollarCountInTransaction { get; private set; }
-        public int FiveDollarCountInTransaction { get; private set; }
-        public int TwentyDollarCountInTransaction { get; private set; }
-
-        public void InsertMoney(
-            int oneCentCount,
-            int tenCentCount,
-            int quartertCentCount,
-            int oneDollarCount,
-            int fiveDollarCount,
-            int twentyDollarCount)
+        public void InsertMoney(Money money)
         {
-            OneCentCountInTransaction += oneCentCount;
-            TenCentCountInTransaction += tenCentCount;
-            QuarterCentCountInTransaction += quartertCentCount;
-            OneDollarCountInTransaction += oneDollarCount;
-            FiveDollarCountInTransaction += fiveDollarCount;
-            TwentyDollarCountInTransaction += twentyDollarCount;
+            MoneyInTransaction += money;
         }
 
         public void ReturnMoney()
         {
-            OneCentCountInTransaction = 0;
-            TenCentCountInTransaction = 0;
-            QuarterCentCountInTransaction = 0;
-            OneDollarCountInTransaction = 0;
-            FiveDollarCountInTransaction = 0;
-            TwentyDollarCountInTransaction = 0;
+            //MoneyInTransaction = 0;
         }
 
         public void BuySnack()
         {
-            OneCentCount += OneCentCountInTransaction;
-            TenCentCount += TenCentCountInTransaction;
-            QuarterCentCount += QuarterCentCountInTransaction;
-            OneDollarCount +=  OneDollarCountInTransaction;
-            FiveDollarCount +=  FiveDollarCountInTransaction;
-            TwentyDollarCount += TwentyDollarCountInTransaction;
+            MoneyInside += MoneyInTransaction;
+            //MoneyInTransaction = 0;
 
-            OneCentCountInTransaction = 0;
-            TenCentCountInTransaction = 0;
-            QuarterCentCountInTransaction = 0;
-            OneDollarCountInTransaction = 0;
-            FiveDollarCountInTransaction = 0;
-            TwentyDollarCountInTransaction = 0;
         }
     }
 }
